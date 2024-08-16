37c904209337e96fbaf0af051fdc182d3f3cc1b9
RJrijo123 <rijojacob2001@gmail.com>
2024-08-16 15:44:02 +0530

    initial commit

---
+++ src/components/Attribution.js
@@ -0,0 +1,7 @@
+export default function Attribution() {
+   return (
+      <footer className="attribution">
+         <p>developed by <a href="http://rjrijo123.github.io/portfolio" target="_blank" rel="noreferrer">Rijo Jacob</a></p>
+      </footer>
+   );
+}
